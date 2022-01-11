f0bf7aeab563634e99ee9fde87869aa3d171e16b
20CE043 <97497245+20CE043@users.noreply.github.com>
2022-01-11 10:43:54 +0530

    Add files via upload

---
+++ practical2_1_d.py
@@ -0,0 +1,10 @@
+#Name: Hena Kharwa
+#ID: 20CE043
+#Github link: https://github.com/20CE043/Practical-2-1.d.git
+#Aim:  Write a Python script to add a key to a dictionary.
+a = {1:1,2:2}
+#adding a key inside a existing dictionary
+a.update({3:3})
+a.update({5:8})
+#printing updated dictionary
+print(a)
